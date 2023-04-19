14ab4a6a102ec7b5c9dbc45896947cbc114375ee
joshberger5 <joshberger5@yahoo.com>
2023-04-19 19:36:30 -0400

    computeTriangles

--- src/preprocessor/TriangleIdentifier.java
+++ src/preprocessor/TriangleIdentifier.java
@@ -1,5 +1,6 @@
 package preprocessor;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.Set;
 import exceptions.FactException;
 import geometry_objects.Segment;
 import geometry_objects.Triangle;
+import geometry_objects.angle.Angle;
 
 public class TriangleIdentifier
 {
@@ -22,7 +24,7 @@ public class TriangleIdentifier
 
 	/*
 	 * Compute the figure triangles on the fly when requested;
-	 * memoize results for subsequent calls.
+	 * memorize results for subsequent calls.
 	 */
 	public Set<Triangle> getTriangles()
 	{
@@ -37,6 +39,22 @@ public class TriangleIdentifier
 
 	private void computeTriangles()
 	{
-		// TODO
+		Segment[] segments = (Segment[]) _segments.keySet().toArray();
+		
+		for(int i=0; i<segments.length-2; i++) {
+			for(int j=i+1; j<segments.length-1; j++) {
+				for (int k=j+1; k<segments.length; k++) {
+					try {
+						List<Segment> segs = new ArrayList<Segment>();
+						segs.add(segments[i]);
+						segs.add(segments[j]);
+						segs.add(segments[k]);
+						Triangle triangle = new Triangle(segs);
+						_triangles.add(triangle);
+					}
+					catch (FactException e) {}	
+				}
+			}
+		}
 	}
 }
