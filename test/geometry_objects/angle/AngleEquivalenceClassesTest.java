259f7bf71086f32a6fa8671a7e8e1272493daca9
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2023-04-20 10:50:02 -0400

    added more test classes

---
+++ test/geometry_objects/angle/AngleEquivalenceClassesTest.java
@@ -0,0 +1,14 @@
+package geometry_objects.angle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import exceptions.FactException;
+import geometry_objects.Segment;
+import geometry_objects.angle.Angle;
+import geometry_objects.points.Point;
+
+public class AngleEquivalenceClassesTest {
+	
+}
---
+++ test/geometry_objects/angle/AngleTest.java
@@ -0,0 +1,15 @@
+package geometry_objects.angle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import exceptions.FactException;
+import geometry_objects.Segment;
+import geometry_objects.angle.Angle;
+import geometry_objects.points.Point;
+
+class AngleTest {
+	
+	
+}
